d71c41cbeb5d6f3669bd607e0713046408ab7210
fatalwastaken <118650632+fatalwastaken@users.noreply.github.com>
2022-11-23 15:05:25 +1100

    Update signup.inc.php

--- signup.inc.php
+++ signup.inc.php
@@ -1,8 +1,25 @@
 <?php
 
 if (isset($_POST["submit"])) {
-	echo "It works";
+
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $username = $_POST["uid"];
+  $pwd = $_POST["pwd"];
+  $pwdRepeat = $_POST["pwdRepeat"];
+
+
+  require_once 'db.inc.php';
+  require_once 'functions.inc.php';
+
+
+  if (emptyInputSignup($name, $email, $username, $pwd, $pwdRepeat) !== false) {
+    header("location: ../signup.html?error=emptyinput");
+	exit();
+  }
+
 }
 else {
-	header("location: ../signup.html")
+	header("location: ../signup.html");
+	exit();
 }
